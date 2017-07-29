49ddc8f76e447fc0fa26dafb3bc0c5b922d2ccd8
Praveen Gupta <pvgupta24@gmail.com>
2017-07-29 23:34:47 +0530

    Basic Login with others.

--- public/javascripts/ctrl.js
+++ public/javascripts/ctrl.js
@@ -1,9 +1,6 @@
 /**
  * Created by Praveen Gupta on 19-07-2017.
  */
-/**
- * Created by Praveen Gupta on 18-06-2017.
- */
 
 /*
 
@@ -141,52 +138,73 @@
  });
  */
 
-app.controller('playlistsCtrl', ['$scope', '$http', '$state', '$rootScope', function ($scope, $http, $state, $rootScope) {
-    var refresh = function () {
-        $http({
-            method: 'GET',
-            url: "/api/playlists"
-        }).then(function success(res) {
-            console.log(res);
-            $scope.playlists = res.data;
-        }, function fail() {
-        });
-    };
-    refresh();
-    $scope.openPlaylist = function (name) {
-        $rootScope.currentPlaylistName = name;
-        $state.go('playlist');
-    };
-    $scope.addPlaylist = function () {
-        $http({
-            method: 'POST',
-            url: "/api/playlists",
-            data: {
-                name: $scope.playlistName,
-                nameOfUser: 'Praveen',
-                username: 'mishal23'
-            }
-        }).then(function success(res) {
-            console.log(res);
-            refresh();
-        }, function fail() {
-        });
-    }
-}]);
+app.controller('playlistsCtrl',
+    ['$scope', '$http', '$state', '$sessionStorage', function ($scope, $http, $state, $sessionStorage) {
+        var refresh = function () {
+            $http({
+                method: 'GET',
+                url: "/api/playlists"
+            }).then(function success(res) {
+                console.log(res);
+                $scope.playlists = res.data;
+            }, function fail() {
+            });
+        };
+        refresh();
+        $scope.openPlaylist = function (name) {
+            $sessionStorage.currentPlaylistName = name;
+            $state.go('playlist');
+        };
+        $scope.addPlaylist = function () {
+            $http({
+                method: 'POST',
+                url: "/api/playlists",
+                data: {
+                    name: $scope.playlistName,
+                    nameOfUser: 'Praveen',
+                    username: 'mishal23'
+                }
+            }).then(function success(res) {
+                console.log(res);
+                refresh();
+            }, function fail() {
+            });
+        }
+    }]);
 
 app.controller('playlistCtrl',
-    ['$scope', '$rootScope', '$http', '$sce', function ($scope, $rootScope, $http, $sce) {
+    ['$scope', '$sessionStorage', '$http', '$sce', function ($scope, $sessionStorage, $http, $sce) {
+
+
+        $scope.trustUrl = function (url) {
+            return $sce.trustAsResourceUrl('https://www.youtube.com/embed/' + url);
+        };
+        $scope.playlistName = $sessionStorage.currentPlaylistName;
+        $scope.playAllButton = function () {
+            $scope.playAllUrl = $scope.songs[0] + "?playlist=";
+
+            for (id in $scope.songs) {
+                if (id > 0)
+                    $scope.playAllUrl += $scope.songs[id];
+            }
+            $scope.playAllUrl += "&autoplay=1";
+        };
         var refreshPlaylist = function () {
 
             $http({
                 method: 'GET',
-                url: '/api/playlists/' + $rootScope.currentPlaylistName
+                url: '/api/playlists/' + $sessionStorage.currentPlaylistName
             }).then(function success(res) {
                 console.log(res);
                 $scope.songs = res.data.songs;
-                for(var i=0;i<$scope.songs.length;i++){
-                    $scope.songs[i]=$sce.trustAsResourceUrl($scope.songs[i]);
-                }
+
+
+                /*for(var i=0;i<$scope.songs.length;i++){
+                 $scope.songs[i].trustedUrl=$sce.trustAsResourceUrl($scope.songs[i]);
+                 console.log($scope.songs[i]);
+                 console.log($scope.songs[i].trustedUrl);
+                 }*/
+
             }, function fail() {
                 console.log('Gadhe');
             });
@@ -197,20 +215,21 @@ app.controller('playlistCtrl',
 
             var link = $scope.songUrl;
             // var id=(link.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/));
-            var id=link.split("/");
-            id=id.pop().split("=");
-             //console.log(id.pop());
-             link = 'https://www.youtube.com/embed/'+id.pop();
-             console.log(link);
+            var id = link.split("&")[0];
+            id = id.split("/");
+            id = id.pop().split("=");
+            //console.log(id.pop());
+            link = id.pop();
+            console.log(link);
 
-          /*   if(id!==null) {
+            /*   if(id!==null) {
              id = id[1].split("/");
 
              if (id.length === 2) {
              //$('#video').show();
              console.log(id[1]);
 
-            ' + id[1];
+             ' + id[1];
              $scope.songUrl = "";
              }
              else if (id.length === 1) {
@@ -221,7 +240,7 @@ app.controller('playlistCtrl',
              }
              $http({
              method: 'POST',
-             url: '/api/playlists/' + $rootScope.currentPlaylistName,
+             url: '/api/playlists/' + $sessionStorage.currentPlaylistName,
              data: {song: $sce.trustAsResourceUrl(link)}
              }).then(function success(res) {
              console.log(res);
@@ -236,36 +255,50 @@ app.controller('playlistCtrl',
              $scope.songUrl = "";
              }
              */
-            if ($scope.songUrl !== null) {
+            if ($scope.songUrl !== null && $scope.songUrl !== '') {
                 $http({
                     method: 'POST',
-                    url: '/api/playlists/' + $rootScope.currentPlaylistName,
+                    url: '/api/playlists/' + $sessionStorage.currentPlaylistName,
                     data: {song: link}
                 }).then(function success(res) {
-                    console.log(res);
-                    $scope.songUrl = '';
+                    //myService.toast("Added");
+                    $scope.songUrl = "";
                     refreshPlaylist();
                 }, function fail() {
                     console.log('yooo');
                 });
 
             } else {
-               // myService.toast("Kuch toh daal gadhe");
+                // myService.toast("Kuch toh daal gadhe");
             }
 
 
         };
 
-        $scope.deleteSong = function (s) {
-            console.log(s);
+        $scope.deleteSong = function (song) {
+            console.log(song);
             $http({
                 method: 'POST',
-                url: '/api/playlists/' + $rootScope.currentPlaylistName + '/delete',
-                data: {song: s}
+                url: '/api/playlists/' + $sessionStorage.currentPlaylistName + '/delete',
+                data: {song: song}
             }).then(function () {
                 refreshPlaylist();
             }, function () {
                 console.log('delete nhi hua');
             });
+        }
+    }]);
+app.controller('loginCtrl',
+    ['$scope', function ($scope) {
+        $scope.loginGoogle = function () {
+            function onSignIn(googleUser) {
+                var profile = googleUser.getBasicProfile();
+                console.log('ID: ' + profile.getId());
+                console.log('Name: ' + profile.getName());
+                console.log('Image URL: ' + profile.getImageUrl());
+                console.log('Email: ' + profile.getEmail());
+            }
+
+            window.onSignIn = onSignIn;
         }
     }]);
--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -3,12 +3,15 @@
  */
 
 app.service('myService', ['$rootScope,$timeout', function ($rootScope,$timeout) {
-    this.toast = function (msg) {
+    var obj = {};
+    obj.toast = function (msg) {
+        console.log('inside toast');
         $myToast = $('#myToast');
         $rootScope.toastMsg = msg;
         $myToast.modal('show');
         $timeout(function () {
             $myToast.modal('hide');
         }, 1500);
+        return obj;
     }
 }]);
